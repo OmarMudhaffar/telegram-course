c1bc5af8c92091739961bb5ecd05dc7cae9aa0c0
OmarReal <real25130@gmail.com>
2017-11-18 02:23:35 +0300

    الايديات الجزء الاول

---
+++ idPartOne.php
@@ -0,0 +1,36 @@
+<?php
+
+// By @Omar_Real 
+// My Channel Set_Web
+ob_start();
+define('API_KEY','TOKEN');
+function bot($method,$datas=[]){
+    $url = 'https://api.telegram.org/bot'.API_KEY.'/'.$method;
+$ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
+$update = json_decode(file_get_contents('php://input'));
+$message = $update->message;
+$chat_id = $message->chat->id;
+$text = $message->text;
+$message_id = $message->message_id;
+$user_id = $message->from->id;
+
+
+if($text == "/start"){
+bot('sendMessage',[
+'chat_id'=>$chat_id,
+'text'=>"chat id is : "  . $chat_id . "\n user id is : " . $user_id,
+'reply_to_message_id'=>$message_id
+]);
+
+}
+
