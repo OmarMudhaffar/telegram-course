673f3ee56518b9f3c45263d664daad31a5cc226d
OmarReal <real25130@gmail.com>
2017-11-18 02:03:58 +0300

    التعامل مع الشروط

---
+++ if-else.php
@@ -0,0 +1,50 @@
+<?php
+
+// By OmarReal 
+// My Channel Set_Web
+
+ob_start();
+define('API_KEY','TOKEN');
+function bot($method,$datas=[]){
+    $url = 'https://api.telegram.org/bot'.API_KEY.'/'.$method;
+$ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
+
+
+
+$update = json_decode(file_get_contents('php://input'));
+$message = $update->message;
+$chat_id = $message->chat->id;
+$text = $message->text;
+
+if($text == '/start'){
+bot('sendMessage',[
+'chat_id'=>$chat_id,
+'text'=>'hello'
+]);
+}
+
+elseif($text == "omar"){
+bot('sendMessage',[
+'chat_id'=>$chat_id,
+'text'=>'welcome'
+]);
+}
+
+else{
+
+bot('sendMessage',[
+'chat_id'=>$chat_id,
+'text'=>'bye'
+	
+}
+
