2daac445b75683d600fafe073a3c01432f521cbc
OmarReal <real25130@gmail.com>
2017-11-18 14:17:12 +0300

    تثبيت الرسائل في المجموعة

---
+++ pinChatMessage.php
@@ -0,0 +1,39 @@
+<?php
+
+// By OmarReal 
+// My Channel Set_Web
+
+ob_start();
+define('API_KEY','TOKEN');
+function bot($method,$datas=[]){
+    $url = 'https://api.telegram.org/bot'.API_KEY.'/'.$method;
+$ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
+$update = json_decode(file_get_contents('php://input'));
+$message = $update->message;
+$chat_id = $message->chat->id;
+$text = $message->text;
+$reply = $message->reply_to_message;
+
+if($text == '/pin' and $reply){
+bot('pinChatMessage',[
+'chat_id'=>$chat_id,
+'message_id'=>$message->reply_to_message->message_id
+]);
+
+bot('sendMessage',[
+'chat_id'=>$chat_id,
+'text'=>'Done',
+'reply_to_message_id'=>$message->reply_to_message->message_id
+]);
+
+}
