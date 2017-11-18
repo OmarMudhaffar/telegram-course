7d0b06252167d74384775d47fcfeb98f80b07b8b
OmarReal <real25130@gmail.com>
2017-11-18 14:09:46 +0300

    طرد عضو من المجموعة

---
+++ kickChatMember.php
@@ -0,0 +1,33 @@
+<?php
+
+// By OmarReal 
+// My Channel Set_Web
+
+
+ob_start();
+define('API_KEY','TOKEN');
+function bot($method,$datas=[]){
+    $url = 'https://api.telegram.org/bot'.API_KEY.'/'.$method;
+$ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
+$update = json_decode(file_get_contents('php://input'));
+$message = $update->message;
+$chat_id = $message->chat->id;
+$text = $message->text;
+$reply = $message->reply_to_message;
+
+if($text == '/kick' and $reply){
+bot('kickChatMember',[
+'chat_id'=>$chat_id,
+'user_id'=>$message->reply_to_message->from->id
+]);
+}
