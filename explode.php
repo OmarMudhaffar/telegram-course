ac01f1fb3d2ed16724bcf624ac1ae4e184b74317
OmarReal <real25130@gmail.com>
2017-11-18 11:39:06 +0300

    شرح دالة القص

---
+++ explode.php
@@ -0,0 +1,34 @@
+<?php
+// By @Omar_Real 
+// My Channel Set_Web
+
+ob_start();
+define('API_KEY','TOKEN');
+function bot($method,$datas=[]){
+    $url = 'https://api.telegram.org/bot'.API_KEY.'/'.$method;
+$ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
+$update = json_decode(file_get_contents('php://input'));
+$message = $update->message;
+$chat_id = $message->chat->id;
+$text = $message->text;
+$message_id = $message->message_id;
+$ex = explode("/echo", $text);
+
+
+if($ex){
+bot('sendMessage',[
+'chat_id'=>$chat_id,
+'text'=>$ex[1],
+'reply_to_message_id'=>$message_id
+]);
+}
